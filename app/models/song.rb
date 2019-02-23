7ed49186b81592ded7baeec60d026e0288f14720
Rachel Wyatt <rw126@evansville.edu>
2019-02-23 14:39:59 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,19 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   belongs_to :genre
 
-  def category_name=(name)
-    self.category ? self.category.name : nil
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
+  end
+
+  def genre_name
+    self.genre ? self.genre.name : nil
   end
 end
